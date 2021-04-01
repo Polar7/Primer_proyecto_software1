d8af8218ea9122aee4a2b7c0795cfde4fc06997f
fenixrage <78386454+fenixrage@users.noreply.github.com>
2021-04-01 14:59:43 -0500

    pajaro

---
+++ Primer_Proyecto_Software1/mundo/pepito/juanito.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LaboratorioMedico.mundo.pepito
+{
+    public partial class juanito : Form
+    {
+        public juanito()
+        {
+            InitializeComponent();
+        }
+    }
+}
